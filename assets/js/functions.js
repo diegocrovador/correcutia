013c437839fbec38ab71cc48ec5411ddacb4245e
Diego Crovador <diego.crovador@gmail.com>
2016-10-05 10:28:14 -0300

    initial commit

---
+++ assets/js/functions.js
@@ -0,0 +1,55 @@
+$(function() {
+  mobileNav();
+
+  $("header h1").fitText(1, { minFontSize: '37px', maxFontSize: '75px' });
+
+  // Closes the Responsive Menu on Menu Item Click
+  //$('.mobile-nav-toggle)').click(function() {
+    //$('.mobile-nav:visible').click();
+  //});
+
+});
+
+// mobile nav
+function mobileNav() {
+  $('.mobile-nav-toggle').on('click', function(){
+    var status = $(this).hasClass('is-open');
+    if(status){ $('.mobile-nav-toggle, .mobile-nav').removeClass('is-open'); }
+    else { $('.mobile-nav-toggle, .mobile-nav').addClass('is-open'); }
+  });
+}
+
+// fitText function
+(function( $ ){
+
+  $.fn.fitText = function( kompressor, options ) {
+
+    // Setup options
+    var compressor = kompressor || 1,
+        settings = $.extend({
+          'minFontSize' : Number.NEGATIVE_INFINITY,
+          'maxFontSize' : Number.POSITIVE_INFINITY
+        }, options);
+
+    return this.each(function(){
+
+      // Store the object
+      var $this = $(this);
+
+      // Resizer() resizes items based on the object width divided by the compressor * 10
+      var resizer = function () {
+        $this.css('font-size', Math.max(Math.min($this.width() / (compressor*10), parseFloat(settings.maxFontSize)), parseFloat(settings.minFontSize)));
+      };
+
+      // Call once to set.
+      resizer();
+
+      // Call on resize. Opera debounces their resize by default.
+      $(window).on('resize.fittext orientationchange.fittext', resizer);
+
+    });
+
+  };
+
+
+})( jQuery );
