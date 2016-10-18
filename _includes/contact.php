d7291d88c9abb55f9dafcbe23af3ec1618efb7bd
Diego Crovador <diego.crovador@gmail.com>
2016-10-18 17:38:37 -0200

    contact

---
+++ _includes/contact.php
@@ -0,0 +1,18 @@
+<section id="contact">
+  <div class="yellow-bar">
+    <h1>Entre em contato!</h1>
+    <div class="contact-info">
+      <p>
+        <img src="/assets/img/send-icon.svg" alt="ícone-enviar"/>
+        contato@correcutia.com.br
+      </p>
+      <p>
+        <img src="/assets/img/phone-icon.svg" alt="ícone-enviar"/>
+        11 5090-1933
+      </p>
+    </div>
+  </div>
+  <div style="height: 200px;">
+
+  </div>
+</section>
